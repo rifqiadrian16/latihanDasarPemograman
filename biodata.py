e5465347ad518f09bab85a8328421d5f5487d413
rifqiadrian16 <160811768+rifqiadrian16@users.noreply.github.com>
2024-07-04 14:01:26 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,10 @@
+nama = "Muhamad Rifqi Adrian"
+kelas = "TI23M"
+jurusan = "Teknik Informatika"
+quote = "Jangan pernah berhenti belajar karena hidup tidak pernah berhenti mengajarkan."
+
+print("Biodata Sederhana")
+print(f"Nama: {nama}")
+print(f"Kelas: {kelas}")
+print(f"Jurusan: {jurusan}")
+print(f"Quote: {quote}")
